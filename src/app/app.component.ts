f7676a36f21820db438a687e854b69e9ffe5da57
Jacob Plumb <jacobplumb@Jacobs-MacBook-Pro.local>
2019-04-29 01:21:40 -0400

    (task) added primeng to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'InSeason';
+
+  constructor(){
+    console.log(`this is the title variable: ${this.title}`)
+  }
 }
